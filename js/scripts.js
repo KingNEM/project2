253ecb0204973d97bf6d1b1ac71d93edfd7217a1
KingNEM <elvismwnd108@gmail.com>
2021-02-23 02:15:21 +0300

    add js scripts to the file

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let dayBorn = parseInt( ( ( (21/4) -2*21-1) + ((5*00/4) ) + ((26*(11+1)/10)) + 17 ) % 7
+)
+console.log(dayBorn)
+let daysofTheWeek = ["Sun","Mon","Tue","Wed","Tue","Wed","Thurs","Fri","Sat"]
+console.log(daysofTheWeek[dayborn])
