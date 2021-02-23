40de8e316ad460f09add482a2307c27f6531854e
KingNEM <elvismwnd108@gmail.com>
2021-02-23 20:04:59 +0300

    add js/scripts.js to the file

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,19 @@
 
-const getName = ]()
-<<<<<<< HEAD
+const getName = () => {
+let Name = document.getElementById
+("Name").value
+let gender = document.getElementById("gender")
+.value// check the gender the user has checked
 let dayBorn = parseInt( ( ( (21/4) -2*21-1) + ((5*00/4) ) + ((26*(11+1)/10)) + 17 ) % 7
 )
+// ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 console.log(dayBorn)
-let daysofTheWeek = ["Sun","Mon","Tue","Wed","Tue","Wed","Thurs","Fri","Sat"]
+const daysofTheWeek = ["Sun","Mon","Tue","Wed","Tue","Wed","Thurs","Fri","Sat"]
 console.log(daysofTheWeek[dayborn])
-=======
-let dayborn =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-let CC = the century DOMStringList
-let YY = the year DOMStringList
-let MM = the month
-let DD = the day of the month
-let mod = %
-let dayborn =  parseInt(( ( (21/4) -2*21-1) + ((5*00/4) ) + ((26*(11+1)/10)) + 17 ) % 7)
-console.log(dayborn)
-let daysofTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-console.log(daysofTheWeek[dayBorn])
->>>>>>> 3dae16bf088ef8bd9b36266c6ab34b175a2709f5
 let dayofTheweek = daysofTheWeek[dayBorn]
-
 const male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 const female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+// introduce control flows
+//if (gender = "male"&& day of the week = "sun" ){
+    // console.log(male[dayBorn])
+// }
