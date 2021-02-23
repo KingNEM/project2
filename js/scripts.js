946381b026820a7446e520b6f65bfac8ab954eb3
KingNEM <elvismwnd108@gmail.com>
2021-02-23 20:24:35 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-let born= Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
-
-let gender = document.getElementById(gender)
-=======
-let dayborn =  Math.round(( ( (21/4) -2*21-1) + ((5*00/4) ) + ((26*(11+1)/10)) + 17 ) % 7)
-console.log(dayborn)
-let daysofTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-console.log(daysofTheWeek[dayBorn])
->>>>>>> 3dae16bf088ef8bd9b36266c6ab34b175a2709f5
-=======
 
 const getName = () => {
 let Name = document.getElementById
@@ -30,4 +17,4 @@ const female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 //if (gender = "male"&& day of the week = "sun" ){
     // console.log(male[dayBorn])
 // }
->>>>>>> 40de8e316ad460f09add482a2307c27f6531854e
+
