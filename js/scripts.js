dba966d0489dc5e39ab7e4d1fe1515680b584f3a
KingNEM <elvismwnd108@gmail.com>
2021-02-24 23:42:38 +0300

    add form elements to te index.html and changes in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 
 const getName = () => {
-let Name = document.getElementById
-("Name").value
-let gender = document.getElementById("gender")
-.value// check the gender the user has checked
+    //CC - is the century digits. For example 1989 has CC = 19
+    //YY - is the Year digits (1989 has YY = 89)
+    //MM -  is the Month
+    //DD - is the Day of the month 
+    //mod - is the modulus function ( % )
+// ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 let dayBorn = parseInt( ( ( (21/4) -2*21-1) + ((5*00/4) ) + ((26*(11+1)/10)) + 17 ) % 7
 )
-// ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 console.log(dayBorn)
-const daysofTheWeek = ["Sun","Mon","Tue","Wed","Tue","Wed","Thurs","Fri","Sat"]
+let daysofTheWeek = ["Sun","Mon","Tue","Wed","Tue","Wed","Thurs","Fri","Sat"]
 console.log(daysofTheWeek[dayborn])
-let dayofTheweek = daysofTheWeek[dayBorn]
+const dayofTheweek = daysofTheWeek[dayBorn]
+let gender = document.getElementById("gender")
+.value// check the gender the user has radiod
 const male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 const female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 // introduce control flows
-//if (gender = "male"&& day of the week = "sun" ){
-    // console.log(male[dayBorn])
+//if (gender === "male"&& day of the week === "Fri" ){
+    // console.log(male[Yaw])
+    
+if (getName == "") {
+    alert("Name must pop out");
+    return wrong;
+}
 // }
 
